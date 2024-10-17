07f654c4b9d713ddd5105228e84a69255ecaab74
luccabibar <luccabibar@gmail.com>
2024-10-17 18:05:07 -0300

    consertando sccripts

--- Assets/Scripts/Fakemon.cs
+++ Assets/Scripts/Fakemon.cs
@@ -5,9 +5,28 @@ public class Fakemon
 
     protected Tipos_Mon tipo, tipoSec;
 
-    private int vida, ataque, defesa, velocidade;
+    protected Atributos_Fakemon atributos;
 
-    private string moveA, moveB, moveC, moveD;
+    protected Moveset_Fakemon moveset;
+
+
+    public Fakemon(
+        string nom, string aut, 
+        Tipos_Mon tp, Tipos_Mon tps,
+        int vd, int atk, int def, int vel,
+        Movimento moveA, Movimento moveB, Movimento moveC, Movimento moveD 
+    ) {
+        Nome = nom;
+        Autor = aut;
+
+        Tipo = tp;
+        TipoSec = tipoSec;
+
+        atributos = new Atributos_Fakemon(9, vd, atk, def, vel);
+
+        moveset = new Moveset_Fakemon(moveA, moveB, moveC, moveD);
+    }
+
 
     public string Nome { get => nome; set => nome = value ?? nome; }
     public string Autor { get => autor; set => autor = value ?? autor; }
@@ -15,16 +34,76 @@ public class Fakemon
     public Tipos_Mon Tipo { get => tipo; set => tipo = value ?? tipo; }
     public Tipos_Mon TipoSec { get => tipoSec; set => tipoSec = value; }
 
-    public int AtribVida { get => vida; set => vida = (value >= 1 && value <= 9) ? value : vida; }
-    public int AtribAtaque { get => ataque; set => ataque = (value >= 1 && value <= 9) ? value : ataque; }
-    public int AtribDefesa { get => defesa; set => defesa = (value >= 1 && value <= 9) ? value : defesa; }
-    public int AtribVeloci { get => velocidade; set => velocidade = (value >= 1 && value <= 9) ? value : velocidade; }
+    public Atributos_Fakemon Atributos { get => atributos; }
 
-    public string MoveA { get => moveA; set => moveA = value ?? moveA; }
-    public string MoveB { get => moveB; set => moveB = value ?? moveB; }
-    public string MoveC { get => moveC; set => moveC = value ?? moveC; }
-    public string MoveD { get => moveD; set => moveD = value ?? moveD; }
+    public Moveset_Fakemon Moveset { get => moveset; }
 }
 
 
+class Atributos_Fakemon
+{
+    protected int vida, ataque, defesa, velocidade;
+    protected int limite;
 
+
+    public Atributos_Fakemon(int lim, int vd, int atk, int def, int vel)
+    {
+        limite = lim;
+        Vida = vd;
+        Ataque = atk;
+        Defesa = def;
+        Velocidade = vel;
+    }
+
+
+    public int Vida { get => vida; set => vida = (value >= 1 && value <= limite) ? value : vida; }
+    public int Ataque { get => ataque; set => ataque = (value >= 1 && value <= ataque) ? value : ataque; }
+    public int Defesa { get => defesa; set => defesa = (value >= 1 && value <= defesa) ? value : defesa; }
+    public int Velocidade { get => velocidade; set => velocidade = (value >= 1 && value <= velocidade) ? value : velocidade; }
+}
+
+
+class Moveset_Fakemon
+{
+    protected Movimento moveA, moveB, moveC, moveD;
+
+
+    public Atributos_Fakemon(Movimento a, Movimento b, Movimento c, Movimento d)
+    {
+        MoveA = a;
+        MoveB = b;
+        MoveC = c;
+        MoveD = d;
+    }
+
+
+    public Movimento MoveA { get => moveA; set => moveA = value ?? moveA; }
+    public Movimento MoveB { get => moveB; set => moveB = value ?? moveB; }
+    public Movimento MoveC { get => moveC; set => moveC = value ?? moveC; }
+    public Movimento MoveD { get => moveD; set => moveD = value ?? moveD; }
+}
+
+
+class Movimento
+{
+    protected string nome;
+    protected Tipos_Mon tipo;
+    protected int poder;
+    protected string descricao;
+
+
+    public Movimento(string nm, Tipos_Mon tp, int pow, string desc)
+    {
+        // nao usa setter pq NAO TEM
+        this.nome = nm;
+        this.tipo = tp;
+        this.poder = pow;
+        this.descricao = desc;
+    }
+
+
+    public string Nome { get => Snome; }
+    public Tipos_Mon Tipo { get => tipo; }
+    public int Poder { get => poder; }
+    public string Descricao { get => descricao; }
+}
