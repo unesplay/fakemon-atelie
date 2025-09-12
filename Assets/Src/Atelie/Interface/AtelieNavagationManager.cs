57616c773b402c5798850022eb1ff4352352764c
LuisG0384 <luisgdm03@gmail.com>
2025-09-11 22:16:15 -0300

    Atelie feito

---
+++ Assets/Src/Atelie/Interface/AtelieNavagationManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AtelieNavigationManager : MenuManagerBehaviour<MenusAtelie>
+{
+}
+
+public enum MenusAtelie
+{
+    ATELIE,
+    OUTRO
+}
